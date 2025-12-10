5c4666b23256e8242895cc3ed40fe13a97fbfcce
xeureka <156827630+xeureka@users.noreply.github.com>
2025-12-10 09:10:00 +0300

    Time: 27 ms (25.14%) | Memory: 12.7 MB (67.08%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        s_counter,t_counter = Counter(s),Counter(t)
+        return s_counter == t_counter
