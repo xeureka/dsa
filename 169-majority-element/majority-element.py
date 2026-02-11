5fafa4e9eb6b4902184785d7e9f3062ed6b96d82
xeureka <156827630+xeureka@users.noreply.github.com>
2026-02-11 09:37:46 +0300

    Time: 7 ms (57.64%) | Memory: 21.1 MB (55.07%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-        maj = ceil(len(nums) / 2)
-        for i,j in counter.items():
-            if j >= maj:
-                return i
+        ans = -1
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                ans = num
+            
+            if ans == num:
+                count += 1
+            else:
+                count -= 1
+        
+        return ans
