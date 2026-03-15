c5630dabe7ee9d3666e621001ca81e2c2fd6826c
xeureka <156827630+xeureka@users.noreply.github.com>
2026-03-15 23:06:12 +0300

    Time: 4 ms (61.53%) | Memory: 19.3 MB (70.30%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,11 +1,20 @@
 class Solution:
-  def getHappyString(self, n: int, k: int) -> str:
-    nextLetters = {'a': 'bc', 'b': 'ac', 'c': 'ab'}
-    q = collections.deque(['a', 'b', 'c'])
+    def __init__(self):
+        self.ans = ""
 
-    while len(q[0]) != n:
-      u = q.popleft()
-      for nextLetter in nextLetters[u[-1]]:
-        q.append(u + nextLetter)
+    def solve(self, length, k, n, chars, s):
+        if length == n:
+            k[0] -= 1
+            if k[0] == 0:
+                self.ans = s
+            return
+        for c in chars:
+            if length == 0 or s[-1] != c:
+                self.solve(length + 1, k, n, chars, s + c)
+                if k[0] == 0:
+                    return 
 
-    return '' if len(q) < k else q[k - 1]
+    def getHappyString(self, n: int, k: int) -> str:
+        self.ans = ""
+        self.solve(0, [k], n, ['a', 'b', 'c'], "")
+        return self.ans
